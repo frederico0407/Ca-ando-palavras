b3c65bdac3bd55cb84d65cc348fc33eeb2529ad2
frederico0407 <frederico.zabot@escola.pr.gov.br>
2025-07-07 12:51:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+import { PALAVRAS_RUINS } from "./palavrasRuins.js";
 
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+
+function mostraPalavrasChave() {
+  const texto = document.querySelector('#entrada-de-texto').value;
+  const campoResultado = document.querySelector('#resultado-palavrachave');
+  const palavrasChave = processaTexto(texto);
+  campoResultado.textContent = palavrasChave.join(", ");
+}
+
+function processaTexto(texto) {
+  let palavras = texto.split(/[^a-zA-ZÀ-ÿ]+/);
+  palavras = palavras.map(p => p.toLowerCase());
+  palavras = tiraPalavrasRuins(palavras);
+  const frequencias = contaFrequencias(palavras);
+  const ordenadas = Object.keys(frequencias).sort((p1, p2) => frequencias[p2] - frequencias[p1]);
+  return ordenadas.slice(0, 10);
+}
