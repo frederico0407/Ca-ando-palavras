4706b7f8c348d7aa46690de83850630187392f7f
frederico0407 <frederico.zabot@escola.pr.gov.br>
2025-07-07 12:52:34 -0300

    Update palavrasRuins.js

--- palavrasRuins.js
+++ palavrasRuins.js
@@ -1,3 +1,7 @@
 export const PALAVRAS_RUINS = new Set([
   'de', 'a', 'o', 'que', 'e', 'do', 'da', 'em', 'um', 'para',
   'é', 'com', 'não', 'uma', 'os', 'no', 'se', 'na', 'por', 'mais',
+  'as', 'dos', 'como', 'mas', 'foi', 'ao', 'ele', 'das', 'tem',
+  'à', 'às', 'lhe', 'eles', 'elas', 'me', 'minha', 'tua', 'teu'
+]);
+
